a2e80081b00643a0a9331aaddf69f4d07c2d9a8a
wfchiang <weifan.wf@gmail.com>
2019-11-20 23:35:51 -0500

    update the human-ctrl-f game to make it generate suare of characters

--- human-ctrl-f.py
+++ human-ctrl-f.py
@@ -9,8 +9,13 @@ CHARS = CHARS + map((lambda x:chr(x)), range(ord('A'), ord('Z')+1))
 
 QUIZS = []
 QUIZS.append(['GOD', 'IS', 'LOVE'])
+QUIZS.append(['FATHER', 'SON', 'SPIRIT'])
+QUIZS.append(['JESUS', 'GOOD', 'SHEPHERD'])
+QUIZS.append(['LAMB', 'OF', 'GOD'])
+QUIZS.append(['BY', 'GRACE', 'THROUGH', 'FAITH'])
 
-OCEAN_SIZE = 500
+OCEAN_WIDTH  = 75 
+OCEAN_HEIGHT = 15
 
 def randomlyCut (n_cuts, victim_list_len): 
     assert(type(n_cuts) is int)
@@ -30,35 +35,57 @@ def randomlyCut (n_cuts, victim_list_len):
     indexs.sort()
     return indexs
 
-def makeCharOcean (n_chars, this_quiz): 
-    assert(type(n_chars) is int)
-    assert(type(this_quiz) is list)
-    assert(n_chars > 0)
+def makeCharOcean (): 
+    cocean = []
 
-    cocean = '' 
-    for i in range(0, n_chars): 
-        cocean += random.choice(CHARS) 
-    
-    cut_indexs = randomlyCut(len(this_quiz), len(cocean))
+    for r in range(0, OCEAN_HEIGHT): 
+        ocean_row = ''
+        for w in range(0, OCEAN_WIDTH): 
+            ocean_row += random.choice(CHARS)
+        cocean.append(ocean_row)
 
-    final_string = ''
-    prev_id = 0
+    return cocean 
 
-    for i in range(0, len(this_quiz)):
-        final_string = final_string + cocean[prev_id:cut_indexs[i]] + this_quiz[i]
-        prev_id = cut_indexs[i]
-    
-    final_string = final_string + cocean[prev_id:]
+def hideTreeIntoForest (forest, tree): 
+    assert(type(forest) is str)
+    assert(type(tree) is str)
 
-    prev_id = -1
+    assert(len(forest) >= len(tree))
+    assert(len(tree) > 0)
 
-    for i in range(0, len(this_quiz)): 
-        this_id = final_string.find(this_quiz[i])
-        assert(this_id >= 0)
-        assert(prev_id < this_id)
-        prev_id = this_id
+    starting_index = random.choice(range(0, (len(forest)-len(tree)+1)))
 
-    return final_string
+    return forest[0:starting_index] + tree + forest[starting_index+len(tree):]
+
+def hideQuizIntoOcean (cocean=[], quiz=[]): 
+    assert(len(cocean) >= len(quiz))
+    assert(len(quiz) > 0)
+
+    candidate_row_indices = []
+    while (len(candidate_row_indices) < len(quiz)): 
+        this_index = random.choice(range(0, len(cocean)))
+        if (this_index not in candidate_row_indices): 
+            candidate_row_indices.append(this_index)
+    candidate_row_indices.sort()
+
+    for i in range(0, len(candidate_row_indices)):
+        forest_index = candidate_row_indices[i]
+        cocean[forest_index] = hideTreeIntoForest(cocean[forest_index], quiz[i])
+
+    return cocean
+
+def validate (cocean, quiz): 
+    row_index = 0
+    for q in quiz:
+        while (True): 
+            if (q in cocean[row_index]): 
+                row_index += 1
+                break
+            else: 
+                row_index += 1
+                if (row_index >= len(cocean)): 
+                    return False
+    return True 
 
 def getQuizString (this_quiz): 
     assert(type(this_quiz) is list)
@@ -73,11 +100,16 @@ def getQuizString (this_quiz):
 # Main 
 # ====
 if __name__ == '__main__': 
-    for i in range(0, 1): 
+    for i in range(0, 10): 
         this_quiz = random.choice(QUIZS)
-        quiz = makeCharOcean(OCEAN_SIZE, this_quiz)
+        cocean = hideQuizIntoOcean(makeCharOcean(), this_quiz)
+
+        assert(validate(cocean, this_quiz))
 
         print (getQuizString(this_quiz))
         print ('')
-        print (quiz)
+        for r in cocean: 
+            print (r)
+        print ('')
+        print ('')
         print ('')
